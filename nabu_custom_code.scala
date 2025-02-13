60cfd5ac087d7c5f5ff610ca42e8a02168b3ddea
Dasarivamshi3857 <114340881+Dasarivamshi3857@users.noreply.github.com>
2025-02-13 18:08:41 +0530

    Update nabu_custom_code.scala

--- nabu_custom_code.scala
+++ nabu_custom_code.scala
@@ -1,11 +1,9 @@
 import com.github.music.of.the.ainur.almaren.builder.Core.Implicit 
 import com.github.music.of.the.ainur.almaren.Almaren 
-import org.apache.spark.sql._
-import java.io._
 
 val almaren = Almaren("Demo1")
 val spark = almaren.spark.getOrCreate()
 
 val df_res = almaren.builder
-.sourceJdbc("jdbc:postgresql://w3.training5.modak.com:5432/postgres","org.postgresql.Driver"," select * from mt24059_fitness_data ",Some("mt24059"),Some("mt24059@m10y24"))
+.sourceJdbc("jdbc:postgresql://w3.training5.modak.com:5432/postgres","org.postgresql.Driver","select * from mt24059_fitness_data",Some("mt24059"),Some("mt24059@m10y24"))
 .targetJdbc("jdbc:postgresql://w3.training5.modak.com:5432/training","org.postgresql.Driver","mt24059_nabu2",SaveMode.Overwrite,Some("mt24059"),Some("mt24059@m10y24"))
